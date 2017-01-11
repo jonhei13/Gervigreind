3cdc26fbdbb911a6e8c50569be87ea1bb2266d3f
jonhei13 <jonhei13@ru.is>
2017-01-11 16:29:45 +0000

    yo

---
+++ ARTI_lab1/src/aiAgent.java
@@ -0,0 +1,23 @@
+public class aiAgent{
+	
+	public void turnLeft(){
+		
+	}
+	public void turnRight(){
+		
+	}
+	public void suck(){
+		
+	}
+	public void go(){
+		
+	}
+	private boolean hitWall(){
+		
+		return true;
+	}
+	private boolean dirt(){
+		
+		return false;
+	}
+}
