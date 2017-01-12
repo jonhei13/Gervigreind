c396b557a1c1f7243628cc67ae460295da698c43
jonhei13 <jonhei13@ru.is>
2017-01-12 13:10:22 +0000

    aiAgent

--- ARTI_lab1/src/aiAgent.java
+++ ARTI_lab1/src/aiAgent.java
@@ -1,30 +1,38 @@
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class aiAgent implements Agent{
 	
-	public void turnLeft(){
-		
-	}
-	public void turnRight(){
-		
-	}
-	public void suck(){
-		
-	}
-	public void go(){
-		
-	}
-	private boolean hitWall(){
-		
-		return true;
-	}
-	private boolean dirt(){
-		
-		return false;
-	}
 	@Override
 	public String nextAction(Collection<String> percepts) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub		
+		String[] actions = { "TURN_ON", "TURN_OFF", "TURN_RIGHT", "TURN_LEFT", "GO", "SUCK" };
+		
+		
+		ArrayList<String> Obsticle;
+		String UnoObsticle;
+		System.out.print("perceiving:");
+		for(String percept:percepts) {
+			System.out.print("'" + percept + "', ");
+			Obsticle.add(percept);
+			UnoObsticle = percept;
+		}
+		if (Obsticle.size() > 1){
+			for (int i = 0; i < Obsticle.size(); i++){
+				//TODO:
+			}
+		}
+		else{
+			if (UnoObsticle == "SUCK"){
+				return actions[5];
+			}
+		}
+		System.out.println("");
+
+		
+		
+		//return actions[random.nextInt(actions.length)];
+	}
 		return null;
 	}
 }
