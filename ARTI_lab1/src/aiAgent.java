ef0cb769cee7f02bbee45977bbeea58e5b0aa2db
Alex <alexi15@ru.is>
2017-01-12 21:28:54 +0000

    private variables in agentAI

--- ARTI_lab1/src/aiAgent.java
+++ ARTI_lab1/src/aiAgent.java
@@ -3,11 +3,18 @@ import java.util.Collection;
 
 public class aiAgent implements Agent{
 	
+	ArrayList<String> preMoves = new ArrayList<String>();
+	private boolean lastTurn;
+	private static boolean RIGHT_TURN = true;
+	private static boolean LEFT_TURN = false;
+	private String lastMove;
+	private static String[] actions = { "TURN_ON", "TURN_OFF", "TURN_RIGHT", "TURN_LEFT", "GO", "SUCK" };
+	
 	@Override
 	public String nextAction(Collection<String> percepts) {
-		// TODO Auto-generated method stub		
-		String[] actions = { "TURN_ON", "TURN_OFF", "TURN_RIGHT", "TURN_LEFT", "GO", "SUCK" };
+		// TODO Auto-generated method stub	
 		
+		String returnAction;
 		
 		ArrayList<String> Obsticle = new ArrayList<String>();
 		String UnoObsticle = "";
@@ -17,6 +24,7 @@ public class aiAgent implements Agent{
 			Obsticle.add(percept);
 			UnoObsticle = percept;
 		}
+		
 		if (Obsticle.size() > 1){
 			for (int i = 0; i < Obsticle.size(); i++){
 				//TODO:
@@ -31,7 +39,6 @@ public class aiAgent implements Agent{
 			}
 			else{
 				return actions[4];
-				
 			}
 		}
 		System.out.println("");
