c16ede4e9d128ef0289373ef5b327bec5e366a3c
Hjálmar Diego Arnórsson <hjalmar15@ru.is>
2017-02-01 10:52:27 +0000

    Added orientation and position, changed State.java

---
+++ Prog1/src/Orientation.java
@@ -0,0 +1,3 @@
+package Prog1;
+
+public enum Orientation {NORTH, EAST, SOUTH, WEST};
--- Prog1/src/State.java
+++ Prog1/src/State.java
@@ -5,11 +5,22 @@ public class State {
 	//orientation
 	//turned on/off
 	//hvaða dirt eru eftir
-	//position//
-	
+	//position
 	//hvaða hlutir breytast
-	
 	//mögulega legal actions úr þessu state-i
 	
+	public Orientation orientation;
+	public Position position;
+	public boolean turned_on;
+
+	public State(Position position, Orientation orientation, boolean turned_on) {
+		this.position = position;
+		this.orientation = orientation;
+		this.turned_on = turned_on;
+	}
+
+	public String toString() {
+		return "State{position: " + position + ", orientation: " + orientation + ", on:" + turned_on + "}";
+	}
 	
 }
